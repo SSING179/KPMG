22e661411582c801083a4ddc7d961e880c153f2e
SSING179 <87057873+SSING179@users.noreply.github.com>
2024-03-22 15:20:14 +0530

    Update provider.tf

--- Challenge 1/Modules/azure-storage-account/provider.tf
+++ Challenge 1/Modules/azure-storage-account/provider.tf
@@ -6,21 +6,3 @@ terraform {
     }
   }
 }
-
-provider "azurerm" {
-  alias = "non-prod"
-  subscription_id = var.non_prod_subscription_id
-  features {}
-}
-
-provider "azurerm" {
-  alias = "staging"
-  subscription_id = var.staging_subscription_id
-  features {}
-}
-
-provider "azurerm" {
-  alias = "prod"
-  subscription_id = var.prod_subscription_id
-  features {}
-}
